66a49dc047fb3cc09b66d8d782dad92c010436cd
shp <yjigess@outlook.com>
2020-07-21 18:40:38 +0800

    updated

--- js/background.js
+++ js/background.js
@@ -26,7 +26,7 @@ chrome.declarativeContent.onPageChanged.removeRules(undefined,function(){
  * purpose: remove duplicate tab 
  * Note: the url properties not set immediate when a tab was created
  */
-chrome.tabs.onCreated.addListener(tab => {
+// chrome.tabs.onCreated.addListener(tab => {
     // chrome.tabs.query({
     //     currentWindow: true,
     //     discarded: false,
@@ -37,30 +37,32 @@ chrome.tabs.onCreated.addListener(tab => {
     //     }
     // });
 
-});
+// });
 
 /**
  * webNavigation onCompleted
- * insert contentScript.js when navigation to bilibili live tabs 
+ * insert run.js when navigation to bilibili live tabs 
+ * 1. if the tab open already then close 
+ * 2. if the tabs more than tab_nums_max then close 
  */
 chrome.webNavigation.onCompleted.addListener(info => {
-if(info.url.includes("live.bilibili")){
-    new Promise((resolve,reject) => {
-        let urls = [];
-        chrome.tabs.query({
-            currentWindow: true,
-            discarded: false,
-            active: false,
-        },tabs => {
-            for(const item of tabs){
-                urls.push(item.url.split("?")[0]);
-            }
-            resolve(urls);
-        });
-    }).then(values => {
-        if(values.indexOf(info.url.split("?")[0]) >= 0 || values.length > tab_nums_max){
-            chrome.tabs.remove(info.tabId);
-        }else{
+    if(info.url.includes("live.bilibili")){
+        new Promise((resolve,reject) => {
+            let urls = [];
+            chrome.tabs.query({
+                currentWindow: true,
+                discarded: false,
+                active: false,
+            },tabs => {
+                for(const item of tabs){
+                    urls.push(item.url.split("?")[0]);
+                }
+                resolve(urls);
+            });
+        }).then(values => {
+            if(values.indexOf(info.url.split("?")[0]) >= 0 || values.length > tab_nums_max){
+                chrome.tabs.remove(info.tabId);
+            }else{
                 chrome.tabs.executeScript(info.tabId,{
                     file: "./js/run.js",
                     runAt: "document_end"
@@ -69,9 +71,9 @@ if(info.url.includes("live.bilibili")){
                         console.log("webNavigation_executeScript: "+chrome.runtime.lastError.message);
                     }
                 });
-        }
-    }).catch(error => {
-        console.log("webNavigation wrong: "+error);
-    });
-}
+            }
+        }).catch(error => {
+            console.log("webNavigation wrong: "+error);
+        });
+    }
 });
